88b52fc4239400ac424fd0929fd1e36d11862bad
iptablecommandcode <iptablecommandcode@gmail.com>
2020-08-31 16:21:36 +0900

    Sign In Layout Finish

---
+++ app/src/main/java/com/example/graduated_work_user_android/Account.java
@@ -0,0 +1,13 @@
+package com.example.graduated_work_user_android;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Account extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
