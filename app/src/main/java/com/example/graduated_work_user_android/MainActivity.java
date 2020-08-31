93c61c9da506ca3f9d73649791ee87b10fa3ee7e
YJ <kyjin0712@naver.com>
2020-08-31 16:12:30 +0900

    리뷰 텍스트와 별점 했습니다.

--- app/src/main/java/com/example/graduated_work_user_android/MainActivity.java
+++ app/src/main/java/com/example/graduated_work_user_android/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.graduated_work_user_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final RatingBar ratingbar = (RatingBar)findViewById(R.id.ratingBar);
+        ratingbar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+            }
+        });
+
+
     }
 }
