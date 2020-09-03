79b5763e957c1c7653404a0b4730e93519de613f
YJ <kyjin0712@naver.com>
2020-09-03 15:17:06 +0900

    account_license 확인

---
+++ app/src/main/java/com/example/graduated_work_user_android/account_license2.java
@@ -0,0 +1,28 @@
+package com.example.graduated_work_user_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class account_license2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //intent
+        Button btnNewActivity = (Button) findViewById(R.id.btnNewActivity);
+        btnNewActivity.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),
+                        Account.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
