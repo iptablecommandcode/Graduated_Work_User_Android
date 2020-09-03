109eddf37b65c5f5c6528edbb7632fdf66d4aa3b
iptablecommandcode <iptablecommandcode@gmail.com>
2020-09-03 15:33:50 +0900

    Account_Lincense_patch

--- app/src/main/java/com/example/graduated_work_user_android/Account.java
+++ app/src/main/java/com/example/graduated_work_user_android/Account.java
@@ -35,13 +35,16 @@ public class Account extends Activity {
         //Button
         BACK = (Button) findViewById(R.id.BACK);
         SAVE = (Button) findViewById(R.id.SAVE);
-
+        LICENSE1 = (Button) findViewById(R.id.LICENSE1);
+        LICENSE2 = (Button) findViewById(R.id.LICENSE2);
+        LICENSE3 = (Button) findViewById(R.id.LICENSE3);
 
         //save
         SAVE.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Toast EmpMsg = Toast.makeText(getApplicationContext(), "값을 입력하십시오", Toast.LENGTH_SHORT);
                 Toast PasMsg = Toast.makeText(getApplicationContext(), "비밀번호가 맞지 않습니다.", Toast.LENGTH_SHORT);
+                Toast ChkMsg = Toast.makeText(getApplicationContext(), "약관에 동의하시기 바랍니다.", Toast.LENGTH_SHORT);
                 Toast CorrMsg = Toast.makeText(getApplicationContext(), "저장", Toast.LENGTH_SHORT);
 
                 //check
@@ -49,15 +52,18 @@ public class Account extends Activity {
                 // return 1 is true
                 // return 2 is empty something
                 // return 3 is password not match
+                // return 4 is password not match
+                // return 5 is password not match
                 AccountCheck accountCheck = new AccountCheck(ID, NAME, PW, PWCHK, PHONE, EMAIL, SCHOOL, NULL);
                 int check = accountCheck.TotalCheck();
                 if(check==1){
-                    //save
                     CorrMsg.show();
                 }else if(check==2){
                     EmpMsg.show();
                 }else if(check==3){
                     PasMsg.show();
+                }else if(check==4){
+                    ChkMsg.show();
                 }
             }
         });
@@ -66,6 +72,30 @@ public class Account extends Activity {
         BACK.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //LICENSE1_Page
+        LICENSE1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), Account_License.class);
+                startActivity(intent);
+            }
+        });
+
+        //LICENSE2_Page
+        LICENSE2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), Account_License2.class);
+                startActivity(intent);
+            }
+        });
+
+        //LICENSE3_Page
+        LICENSE3.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), Account_License3.class);
                 startActivity(intent);
             }
         });
