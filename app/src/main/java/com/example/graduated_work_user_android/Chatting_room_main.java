80a4c0dd8b7fc985778d1fcd8b3a34e7fc1e560e
chsy411 <jsyoon411@naver.com>
2020-09-08 16:39:18 +0900

    chatting_room_main에 adapter 형식 추가, chatting_room_adapter 추가

--- app/src/main/java/com/example/graduated_work_user_android/Chatting_room_main.java
+++ app/src/main/java/com/example/graduated_work_user_android/Chatting_room_main.java
@@ -3,12 +3,15 @@ package com.example.graduated_work_user_android;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class Chatting_room_main extends Activity {
 
     Button backspace;
+    ListView chatlist;
 
     @Override
 
@@ -17,6 +20,7 @@ public class Chatting_room_main extends Activity {
         setContentView(R.layout.chatting_room_main);
 
         backspace = findViewById(R.id.backspace);
+        chatlist = findViewById(R.id.chatlist);
 
         backspace.setOnClickListener(new View.OnClickListener() {
 
@@ -25,6 +29,18 @@ public class Chatting_room_main extends Activity {
 
                 Intent intent = new Intent(Chatting_room_main.this, Chatting_Matching_Review.class);
                 startActivity(intent);
+            }
+
+                @Override
+
+            //형식만 짜둠 수정해야됨
+            public boolean onCreateOptionsMenu(Menu menu) {
+
+                getMenuInflater().inflate(R.menu.chatting_room_adapter, menu);
+
+                chatlist = new Chatting_room_adapter(this.getApplicationContext(),R.layout.chatting_room_main);
+                final ListView listView = findViewById(R.id.chatlist);
+                listView.setAdapter();
             }
         });
     }
