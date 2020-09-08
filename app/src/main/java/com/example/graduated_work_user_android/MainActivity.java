bd879023730014407dc4b4a8b91ddf4b8aab0669
whddns5614 <dlwhddns1001@naver.com>
2020-09-08 15:35:04 +0900

    메인페이지 자바

--- app/src/main/java/com/example/graduated_work_user_android/MainActivity.java
+++ app/src/main/java/com/example/graduated_work_user_android/MainActivity.java
@@ -18,6 +18,15 @@ public class MainActivity extends Activity {
 
         //Button
         Sign_In = (Button) findViewById(R.id.Sign_In);
+        Button chicken = (Button) findViewById(R.id.chicken);
+        Button pizza = (Button) findViewById(R.id.pizza);
+        Button pasta = (Button) findViewById(R.id.pasta);
+        Button koreanfood = (Button) findViewById(R.id.koreanfood);
+        Button chinesefood = (Button) findViewById(R.id.chinesefood);
+        Button japanesefood = (Button) findViewById(R.id.japanesefood);
+        Button Review = (Button) findViewById(R.id.Review);
+
+
 
         Sign_In.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
@@ -25,6 +34,72 @@ public class MainActivity extends Activity {
                 startActivity(intent);
             }
         });
+
+        chicken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //채팅로딩을 만들기 전이라 Account로 이동하게 해놨음 추후 Chatting_loding으로 변경
+                Intent intent = new Intent(getApplicationContext(), Account.class);
+                startActivity(intent);
+            }
+        });
+
+        pizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //채팅로딩을 만들기 전이라 Account로 이동하게 해놨음 추후 Chatting_loding으로 변경
+                Intent intent = new Intent(getApplicationContext(), Account.class);
+                startActivity(intent);
+            }
+        });
+
+        pasta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //채팅로딩을 만들기 전이라 Account로 이동하게 해놨음 추후 Chatting_loding으로 변경
+                Intent intent = new Intent(getApplicationContext(), Account.class);
+                startActivity(intent);
+            }
+        });
+
+        koreanfood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //채팅로딩을 만들기 전이라 Account로 이동하게 해놨음 추후 Chatting_loding으로 변경
+                Intent intent = new Intent(getApplicationContext(), Account.class);
+                startActivity(intent);
+            }
+        });
+
+        chinesefood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //채팅로딩을 만들기 전이라 Account로 이동하게 해놨음 추후 Chatting_loding으로 변경
+                Intent intent = new Intent(getApplicationContext(), Account.class);
+                startActivity(intent);
+            }
+        });
+
+        japanesefood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //채팅로딩을 만들기 전이라 Account로 이동하게 해놨음 추후 Chatting_loding으로 변경
+                Intent intent = new Intent(getApplicationContext(), Account.class);
+                startActivity(intent);
+            }
+        });
+
+        Review.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Chatting_Matching_Review.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
         DB_Conn db = new DB_Conn();
         db.DBConn();
     }
