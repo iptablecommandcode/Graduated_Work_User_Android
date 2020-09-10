88df21c7f5a4520f28ab5ab802f3f851736b9670
whddns5614 <dlwhddns1001@naver.com>
2020-09-10 16:55:34 +0900

    로그인페이지 자바2

--- app/src/main/java/com/example/graduated_work_user_android/LogInPage.java
+++ app/src/main/java/com/example/graduated_work_user_android/LogInPage.java
@@ -1,22 +1,41 @@
 package com.example.graduated_work_user_android;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LogInPage extends Activity {
 
-    EditText id, pw;
-    Button account, login;
+    EditText id,pw;
+    Boolean loginChecked;
+    SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        id = (EditText) findViewById(R.id.);
-        passwordInput = (EditText) findViewById(R.id.passwordInput);
+        setContentView(R.layout.login_page);
+        id = (EditText) findViewById(R.id.edit1);
+        pw = (EditText) findViewById(R.id.edit2);
+    }
+
+
+
+    private boolean loginValidation(String id, String pw) {
+        if(pref.getString("id","").equals(id) && pref.getString("pw","").equals(pw)) {
+            // login success
+            return true;
+        } else if (pref.getString("id","").equals(null)){
+            // sign in first
+            Toast.makeText(LogInPage.this, "Please Sign in first", Toast.LENGTH_LONG).show();
+            return false;
+        } else {
+            // login failed
+            return false;
+        }
     }
 }
+
 
 
