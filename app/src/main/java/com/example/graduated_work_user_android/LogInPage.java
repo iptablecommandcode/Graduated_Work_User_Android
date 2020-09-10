c108b77d6bc95307f90b09d09f3c831f0acc0149
whddns5614 <dlwhddns1001@naver.com>
2020-09-10 17:14:13 +0900

    로그인페이지 자바3

--- app/src/main/java/com/example/graduated_work_user_android/LogInPage.java
+++ app/src/main/java/com/example/graduated_work_user_android/LogInPage.java
@@ -1,8 +1,11 @@
 package com.example.graduated_work_user_android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,21 +21,38 @@ public class LogInPage extends Activity {
         setContentView(R.layout.login_page);
         id = (EditText) findViewById(R.id.edit1);
         pw = (EditText) findViewById(R.id.edit2);
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        Button btn2 = (Button) findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Account.class);
+                startActivity(intent);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginValidation(id.getText().toString(),pw.getText().toString());
+            }
+        });
     }
 
 
 
-    private boolean loginValidation(String id, String pw) {
+    private void loginValidation(String id, String pw) {
         if(pref.getString("id","").equals(id) && pref.getString("pw","").equals(pw)) {
             // login success
-            return true;
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
         } else if (pref.getString("id","").equals(null)){
             // sign in first
             Toast.makeText(LogInPage.this, "Please Sign in first", Toast.LENGTH_LONG).show();
-            return false;
+
         } else {
-            // login failed
-            return false;
+            // login faile;
         }
     }
 }
